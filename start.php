50a574bbf26bacf0810d74cd5a941ef22e121dbc
Germanaz0 <germanazo@gmail.com>
2012-06-27 19:37:25 -0300

    Finished plugin

--- start.php
+++ start.php
@@ -8,16 +8,14 @@
 elgg_register_event_handler('init', 'system', 'wmr_init');
 
 function wmr_init() {
-    /** 
+    /**
      * Register the event when plugin is deactivated
      * @FIXME: This will not work as desired.
      * The wanted result, should be, after flush cache when the module is disabled
      * 
      * ElggCore package do not support an event or a plugin hook when cache is clear
      */
-    
 //    elgg_register_event_handler('deactivate', 'plugin', 'wmr_on_deactivate_plugin');
-
     //Register a cron to fix the river items
     elgg_register_plugin_hook_handler('cron', 'daily', 'wmr_fix_river_items');
 
@@ -30,7 +28,7 @@ function wmr_init() {
     ));
 
     elgg_register_page_handler('watch_my_river', 'wmr_page_handler');
-    
+
     run_function_once('wmr_create_database');
 }
 
@@ -51,7 +49,7 @@ function wmr_page_handler($page) {
  * @return boolean 
  */
 function wmr_create_database() {
-    $schema_file = elgg_get_plugins_path().'watch_my_river/schema/mysql.sql';
+    $schema_file = elgg_get_plugins_path() . 'watch_my_river/schema/mysql.sql';
     run_sql_script($schema_file);
     return TRUE;
 }
@@ -83,7 +81,8 @@ function wmr_river_callback($row) {
     $wmr_ob->subtype = $row->subtype;
     $wmr_ob->view = $view;
 
-    if (elgg_view_exists($view)) {
+    //Check if a view exist, without check extensions
+    if (elgg_view_exists($view, '', FALSE)) {
         $wmr_ob->disable_items = 'no';
     } else {
         $wmr_ob->disable_items = 'yes';
@@ -97,16 +96,96 @@ function wmr_river_callback($row) {
  */
 function wmr_fix_river_items() {
     $dbprefix = elgg_get_config('dbprefix');
+    
+    //Get enabled river items
     $query_get_rivers = "SELECT * FROM {$dbprefix}river GROUP BY view";
-
     $river_views = get_data($query_get_rivers, 'wmr_river_callback');
 
-    /**
-     * @TODO: Make some process to hide or to remove the items, maybe move them
-     * to a tmp table
-     *  But instead make a tmp table, should be great 
-     * that river items has disable/enable support, like every entities
-     */
-    
+    //Get disabled river items
+    $query_get_disabled_rivers = "SELECT * FROM {$dbprefix}river_disabled GROUP BY view";
+    $disabled_river_views = get_data($query_get_disabled_rivers, 'wmr_river_callback');
+
+    //If there are some empty river item, then move to disabled table
+    if (is_array($river_views)) {
+        foreach ($river_views as $wmr_ob) {
+            if ($wmr_ob->disable_items == 'yes') {
+                //Move them to river_disabled
+                wmr_move_to_disabled($wmr_ob->view);
+            }
+        }
+    }
+
+    //If the view on disabled river items exists, then enable
+    if (is_array($disabled_river_views)) {
+        foreach ($disabled_river_views as $disabled_view) {
+            if ($wmr_ob->disable_items == 'no') {
+                //Move them to river_disabled
+                wmr_move_to_enabled($disabled_view->view);
+            }
+        }
+    }
+
     return $river_views;
+}
+
+/**
+ * Move a view into river_disabled table
+ * 
+ * @param string $view
+ * @return bool 
+ */
+function wmr_move_to_disabled($view) {
+    $db_prefix = elgg_get_config('dbprefix');
+
+    $to_table = $db_prefix . 'river_disabled';
+    $from_table = $db_prefix . 'river';
+
+    return wmr_generic_move($to_table, $from_table, $view);
+}
+
+/**
+ * Move a view into river table
+ * 
+ * @param string $view
+ * @return bool 
+ */
+function wmr_move_to_enabled($view) {
+    $db_prefix = elgg_get_config('dbprefix');
+
+    $from_table = $db_prefix . 'river_disabled';
+    $to_table = $db_prefix . 'river';
+
+    wmr_generic_move($to_table, $from_table, $view);
+}
+
+
+/**
+ * Generic function to move items into tables
+ * 
+ * @param string $to_table
+ * @param string $from_table
+ * @param string $view
+ * @return boolean 
+ */
+function wmr_generic_move($to_table, $from_table, $view) {
+    if (empty($view) || empty($from_table) || empty($to_table)) {
+        return FALSE;
+    }
+
+    $query = "INSERT INTO `{$to_table}` SELECT * FROM `{$from_table}` WHERE view = '{$view}'";
+
+    $success = FALSE;
+
+    try {
+        $success = insert_data($query);
+    } catch (Exception $e) {
+        return FALSE;
+    }
+
+    if ($success) {
+        $delete_query = "DELETE FROM `{$from_table}` WHERE `{$from_table}`.`view` = '{$view}'";
+        return delete_data($delete_query);
+    }
+
+    return $success;
 }
